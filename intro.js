5486abb3668a4bbd474ae14814ac1813b814db98
Bigboss <bigbosses00@gmail.com>
2022-06-23 23:21:43 +0100

    Intro

---
+++ intro.js
@@ -0,0 +1,11 @@
+function func1(){}
+    func1.displayName="Enwere Emmanuel"
+    console.log(func1.displayName)
+
+function func2(){}
+    func2.displayName="Height= 1.65"
+    console.log(func2.displayName)
+
+function func3(){}
+    func3.displayName="Nigeria"
+    console.log(func3.displayName)
